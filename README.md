f156397e6839eb0a216d0644722ee46716bf757b
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-11-03 20:52:10 +0100

    Update Readme.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+
+02/11/2022
+I solved the Pascal Triangle Problem on [LeetCode](leetcode.com)
+The Worst case time complexity is O(n2)
+Looking through other solutions, they also have runtime of O(n2) except if the triangle arrays are hardcoded whereby the algorithm have a runtime of O(1)
+
 These repo contain Algorithm exercises on various websites such as
 [LeetCode](leetcode.com)
 running_sum.py : Calculates the running sum of an array of int
---
+++ leetcode/pascal_triangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows < 1:
+            return None
+        pascal_triangle = []
+        pascal_triangle.append([1])
+        for i in range(1,numRows):
+            
+            pascal_triangle.append([1])
+            pascal_triangle[i].append(1)
+            for j in range(1, i):
+                
+                val = pascal_triangle[i-1][j-1] + pascal_triangle[i-1][j]
+                pascal_triangle[i].insert(j, val)
+        return pascal_triangle
